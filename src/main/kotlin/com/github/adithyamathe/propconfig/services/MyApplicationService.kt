dbe26ae3f0fbaf578270ebdfb7b0c3f40926b80f
GitHub Action <action@github.com>
2023-03-08 02:35:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/adithyamathe/propconfig/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adithyamathe/propconfig/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adithyamathe.propconfig.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adithyamathe.propconfig.MyBundle
 
 class MyApplicationService {
 
